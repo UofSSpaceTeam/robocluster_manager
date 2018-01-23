1eb8314613e4e268d697c1d4d03de99c3b27cb19
Ikram Omar Salim <ikramomar998@gmail.com>
2018-01-23 17:35:03 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,9 +4,9 @@ from ProcessManager import ProcessManager, RunOnce
 PATH = './examples/demo'
 
 process_list = [
-    RunOnce("random-stream", f'python {PATH}/random_stream.py'),
-    RunOnce("printer", f'python {PATH}/printer.py'),
-    # RunOnce("serialtest", f'python {PATH}/serial_test.py'),
+    RunOnce("random-stream", 'python {}.format/random_stream.py'.format(PATH)),
+    RunOnce("printer", 'python {}.format/printer.py'.format(PATH)),
+    # RunOnce("serialtest", f'python {PATH}.format/serial_test.py'),
 ]
 
 
